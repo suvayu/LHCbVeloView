f8388eef046b4165524fffc0e0c2f4ea7a8759ad
Manuel Tobias Schiller <mala@hinterbergen.de>
2013-05-03 17:42:15 +0200

    (re)write demo code for modifying a DQ tree

--- testtree.py
+++ testtree.py
@@ -5,49 +5,62 @@ ROOT.gSystem.Load('libCintex')
 ROOT.Cintex.Enable()
 ROOT.gSystem.Load('libVeloGUIUtils')
 
-from TypeHelper import getTypeFactory
+# construct list of valid Velo sensor numbers
+sensors = tuple(xrange(0, 42))
+sensors += tuple(xrange(64, 64 + 42))
+sensors += tuple(xrange(128, 132))
 
-def fillTree(filename, treename):
-    from ROOT import TFile, TTree, TRandom3
+def createTree(filename, treename):
+    from ROOT import TFile, TRandom3
     from ROOT import DotLock, TimeStamp, VersionedObject, std
     from GUITree import Tree
-    sensors = []
-    sensors += [ i for i in xrange(0, 42) ]
-    sensors += [ i for i in xrange(64, 64 + 42) ]
-    sensors += [ i for i in xrange(128, 132) ]
+    # get RNG to generate some data to put into the tuple
     rnd = TRandom3()
+    # acquire write lock
     dl = DotLock(filename)
+    # open file for writing
     f = TFile(filename, 'RECREATE')
-    t = Tree(treename, {
-	# branch names and types
+    f.SetCompressionSettings(ROOT.CompressionSettings(ROOT.kLZMA, 1))
+    # create tree with given structure
+    t = Tree(treename, 'Velo DQ Tree prototype', {
+	# define branch names and types
 	'runnr':	'UInt_t',
+	'checked':	'VersionedObject<UShort_t, TimeStamp, std::greater<TimeStamp> >',
 	'comment':	'VersionedObject<std::string, TimeStamp, std::greater<TimeStamp> >',
-	'meanpedestal':	'VersionedObject<double, TimeStamp, std::greater<TimeStamp> >',
-	'occupancy':	'VersionedObject<std::map<int,std::vector<double> >, TimeStamp, std::greater<TimeStamp> >'
+	'meanpedestal':	'VersionedObject<Float_t, TimeStamp, std::greater<TimeStamp> >',
+	'occupancy':	'VersionedObject<std::map<int,std::vector<Float_t> >, TimeStamp, std::greater<TimeStamp> >'
 	})
-    print 'File and tree open.'
-    for i in xrange(0, 10):
-	print 'Filling run %u' % i
+    # fill tree
+    for i in xrange(0, 64):
+	# clear out data from last run
 	t.comment.clear()
 	t.occupancy.clear()
 	t.meanpedestal.clear()
 
-	t.runnr = i
+	# fill new run: step 1: generate time stamp (now)
 	now = TimeStamp()
-	t.comment[now] = 'initial DQ for run %u' % i
-	t.meanpedestal[now] = -5. + 10. * rnd.Rndm()
+	# fill branches
+	t.runnr = 100000 + i
+	print 'Generating dummy DQ data for run %u' % t.runnr
+	t.checked[now] = 0
+	t.comment[now] = 'initial DQ for run %u: UNCHECKED' % t.runnr
+	t.meanpedestal[now] = rnd.Uniform(-5., 5.)
+	# simulate about 3 permille dead, 5 permille noisy channels
+	fdead = rnd.Uniform(0.003, 0.002)
+	if fdead < 0.: fdead = 0.
+	fnoisy = rnd.Uniform(0.005,0.002)
+	if fnoisy < 0.: fnoisy = 0.
 	for sensor in sensors:
 	    # vector of per-strip occupancies
-	    ov = std.vector('double')(2048)
+	    ov = std.vector('float')(2048)
 	    for strip in xrange(0, 2048):
 		# simulate dead, noisy and normal strips
-		#
-		# note: one has a small bug... ;)
-		tmp = rnd.Uniform()
-		if tmp < 0.0025: ov[strip] = 0.
-		elif tmp < 0.0075: ov[strip] = -rnd.Uniform()
-	        else: ov[strip] = -rnd.Gaus(0.01,0.0025)
-	    # fill that vector into the current version
+		tmp = rnd.Uniform(0., 1.)
+		if tmp > (fdead + fnoisy): ov[strip] = rnd.Gaus(0.01, 0.0025)
+		elif tmp > fnoisy: ov[strip] = rnd.Uniform(0., 1.)
+	        else: ov[strip] = 0.
+	    # put that strip vector for the current sensor into the current
+	    # version
 	    t.occupancy[now][sensor] = ov
 	t.Fill()
     t.Write()
@@ -56,32 +69,64 @@ def fillTree(filename, treename):
     del f
     del dl
 
-def modtree(filename, treename, evnrv, data1v, data2v, data3v):
-    from ROOT import TFile, TTree, TRandom3
-    from ctypes import c_double, c_int
-    rnd = TRandom3()
+def anatree(filename, treename):
+    from ROOT import TFile, TRandom3
+    from ROOT import DotLock, TimeStamp, VersionedObject, std
+    from GUITree import Tree
+    print "Doing dummy DQ on file %s, tree %s" % (filename, treename)
+    # acquire write lock
+    dl = DotLock(filename)
+    # open file for writing
     f = TFile(filename, 'READ')
-    t = f.Get(treename)
+    t = Tree('testtree')
     ff = TFile('%s.new' % filename, 'RECREATE')
-    # copy all but the entry we want to change
-    ct = t.CopyTree('evnr != %u' % evnrv, '', t.GetEntries(), 0)
-    evnr = c_int(0)
-    data1 = c_double(0.)
-    data2 = c_double(0.)
-    data3 = c_double(0.)
-    t.SetBranchAddress('evnr', evnr)
-    t.SetBranchAddress('data1', data1)
-    t.SetBranchAddress('data2', data2)
-    t.SetBranchAddress('data3', data3)
+    ff.SetCompressionSettings(ROOT.CompressionSettings(ROOT.kLZMA, 1))
+    tt = t.CloneTree(0)
     for i in xrange(0, t.GetEntries()):
-	t.GetEntry(evnrv)
-	if evnr.value != evnrv: continue
-	data1.value = data1v
-	data2.value = data2v
-	data3.value = data3v
-	ct.Fill()
-    ct.Write()
-    del ct
+	t.GetEntry(i)
+	print 'Analysing run %u: %s (%s)' % (
+		t.runnr, t.comment.value(), t.comment.active_version().toString())
+	# work out average occupancy in run
+	avgocc = 0.
+	nstrips = 0
+	for sensor in sensors:
+	    ov = t.occupancy.value()[sensor]
+	    avgocc += sum(ov)
+	    nstrips += ov.size()
+	avgocc = avgocc / float(nstrips)
+	print '\taverage occupancy %4.2f%%' % (100. * avgocc)
+	# find dead/noisy strips
+	dead = ()
+	noisy = ()
+	thresh_noise = 3. * avgocc
+	thresh_dead = 0.1 * avgocc
+	for sensor in sensors:
+	    ov = t.occupancy.value()[sensor]
+	    strip = 0
+	    for occ in ov:
+		if thresh_noise < occ:
+		    noisy += ((sensor, strip), )
+		elif thresh_dead > occ:
+		    dead += ((sensor, strip),)
+		strip = strip + 1
+	pdead = 100. * float(len(dead)) / float(nstrips)
+	pnoisy = 100. * float(len(noisy)) / float(nstrips)
+	print '\t dead channels (%4.2f%%)' % pdead
+	print '\tnoisy channels (%4.2f%%)' % pnoisy
+	# set the run OK threshold at 0.75% dead + noisy channels max.
+	now = TimeStamp()
+	if pdead + pnoisy < 0.75:
+	    tt.comment[now] = 'DQ for run %u: OK' % tt.runnr
+	    tt.checked[now] = 2
+	else:
+	    tt.comment[now] = 'DQ for run %u: BAD' % tt.runnr
+	    tt.checked[now] = 1
+	print 'Analysed run %u: %s (%s)' % (
+		tt.runnr, tt.comment.value(), tt.comment.active_version().toString())
+	# write to new tree
+	tt.Fill()
+    tt.Write()
+    del tt
     ff.Close()
     del ff
     del t
@@ -89,6 +134,10 @@ def modtree(filename, treename, evnrv, data1v, data2v, data3v):
     del f
     from os import rename
     rename('%s.new' % filename, filename)
+    del dl
 
-fillTree('test.root', 'testtree')
-#modtree('test.root', 'testtree', 42, 42., 17., 78.)
+print 'Creating DQ tree'
+createTree('test.root', 'testtree')
+print ''
+print 'Analysing DQ tree'
+anatree('test.root', 'testtree')
