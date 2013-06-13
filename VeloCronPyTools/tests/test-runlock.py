4885412b37b9594366b08e0661772d7b777ce7de
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-06-13 17:51:55 +0200

    Update tests to be in sync with RunLock
    1. No need for unsupported stream test because of (8f00a89)
    2. Bad run number test still valid since run number < 1E5 (0f7e438)
    3. Skip permission problem test
    4. Minor attribute renaming for semantic consistency

--- VeloCronPyTools/tests/test-runlock.py
+++ VeloCronPyTools/tests/test-runlock.py
@@ -18,38 +18,31 @@ import unittest
 class TestExceptions(unittest.TestCase):
 
     def setUp(self):
+        self.stream = 'FOO'
         self.good_runno = 137259
-        self.good_stream = 'NZS'
         self.bad_runno = 2.0
-        self.bad_stream = 'FOO'
 
     def test_bad_run_number(self):
         self.assertRaises(UndefinedRunLock, RunLock, self.bad_runno,
-                          self.good_stream)
-
-    def test_unsupported_stream(self):
-        self.assertRaises(UndefinedRunLock, RunLock, self.good_runno,
-                          self.bad_stream)
-
+                          self.stream)
 
     def test_existing_lock_files(self):
         def __existing_lock_files__():
             """Internal method."""
-            with RunLock(self.good_runno, self.good_stream):
-                with RunLock(self.good_runno, self.good_stream):
+            with RunLock(self.good_runno, self.stream):
+                with RunLock(self.good_runno, self.stream):
                     pass
         self.assertRaises(RunLockExists, __existing_lock_files__)
 
-
+    @unittest.skip('Test not finalised')
     def test_permission_problems(self):
-        return
         # FIXME: write to non-existent directory (how?)
         def __permission_problems__():
             """Try acquiring a lock in a read only directory and fail."""
             __tmpdir__ = os.path.join(os.getcwd(), 'tmp')
             os.mkdir(__tmpdir__, 0555)
             with os.chdir(__tmpdir__):
-                with RunLock(self.good_runno, self.good_stream):
+                with RunLock(self.good_runno, self.stream):
                     pass
         self.assertRaises(OSError, __permission_problems__)
 
