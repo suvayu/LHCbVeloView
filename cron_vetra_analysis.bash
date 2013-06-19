4404ce9bdd05996ed5f58940f70876b9b1e85e6a
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2013-06-19 17:06:15 +0200

    Clean up cron script comments

--- cron_vetra_analysis.bash
+++ cron_vetra_analysis.bash
@@ -12,20 +12,21 @@ declare jobargs="$@"
 shift $#
 
 
-# environment: LbLogin et al.
-# initialise environment
+# initialise environment; LbLogin et al.
 source /sw/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v6r1p3/InstallArea/scripts/LbLogin.sh
 echo ${LBSCRIPTS_HOME}
 
 SETSHIFTERVERSION=`grep "export SHIFTERVETRAVERSION" /group/velo/sw/scripts/velo_login.sh`
 eval $SETSHIFTERVERSION
 echo "shifter vetra version" $SHIFTERVETRAVERSION
-
-
-# resource limits: ulimit stuff
 source /sw/lib/lhcb/VETRA/VETRA_${SHIFTERVETRAVERSION}/Velo/VetraScripts/scripts/setup_shifters.sh
+
+
+# set resource limits
 ulimit -Sm 2000000
 ulimit -Sv 2000000
 echo "Running on:" $HOSTNAME
 
+
+# run job
 exec python $srcdir/vetra_analysis.py -c $jobargs
