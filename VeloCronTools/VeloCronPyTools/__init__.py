d92e05d6fcc9039fc2d3ec3d968c88648453039c
Suvayu Ali <Suvayu.Ali@cern.ch>
2013-06-22 18:00:58 +0200

    Package setup for Oracle database

--- VeloCronTools/VeloCronPyTools/__init__.py
+++ VeloCronTools/VeloCronPyTools/__init__.py
@@ -5,3 +5,8 @@ __hostname__ = gethostname()
 if __hostname__.find('plus') == 0:
     import sys, os
     sys.path.insert(0, os.path.join('/group/online/rundb/RunDatabase/python'))
+    try:
+        ORACLE_HOME = os.environ['ORACLE_HOME']
+    except:
+        ORACLE_HOME = None
+        sys.path.append(str(ORACLE_HOME) + "/python")
