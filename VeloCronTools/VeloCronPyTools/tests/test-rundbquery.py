37e11061a20c028d1063ef2bf5d67643f76c8e3f
Suvayu Ali <Suvayu.Ali@cern.ch>
2013-07-08 20:52:11 +0200

    Change tests to reflect switch to rdbt from rundb.RunDB

--- VeloCronTools/VeloCronPyTools/tests/test-rundbquery.py
+++ VeloCronTools/VeloCronPyTools/tests/test-rundbquery.py
@@ -7,9 +7,13 @@ if __file__.startswith('/'):
     # FIXME: clean up
     sys.path.insert(0, os.path.join('/', *__file__.split('/')[:-3]))
 else:
-    __path_to_script__ = __file__.split('/')[:-1] # test directory
-    __path_to_script__ += ['..', '..']            # package directory parent
-    sys.path.insert(0, os.path.join(os.getcwd(), *__path_to_script__))
+    __pkg_dir__ = __file__.split('/')[:-1] # test directory
+    __pkg_dir__ += ['..', '..']            # package directory parent
+    sys.path.insert(0, os.path.join(os.getcwd(), *__pkg_dir__))
+
+__test_dir__ = os.path.dirname(__file__) # test directory
+if __test_dir__ == '':
+    __test_dir__ = './'
 
 from socket import gethostname
 __hostname__ = gethostname()
@@ -18,30 +22,31 @@ __hostname__ = gethostname()
 from VeloCronPyTools.rundbquery import (RunDBQuery)
 import unittest
 
-
-@unittest.skipIf(__hostname__.find('plus') != 0,
-                 'rundb.RunDB is not supported outside plus* nodes')
 class TestQuery(unittest.TestCase):
 
     def setUp(self):
         self.good_runno = 137259
         self.good_runlist = range(137250, 137300)
         self.bad_runno = 150000
+        if __hostname__.find('plus') == 0:
+            self.cmd = ['rdbt', '-n', '137259']
+        else:
+            self.cmd = ['cat', __test_dir__ + '/rdbt_single_run.txt']
 
     def test_good_run_number(self):
-        query = RunDBQuery(self.good_runno)
+        query = RunDBQuery(self.good_runno, self.cmd)
         self.assertTrue(query.get_valid_runs(1800))
 
     def test_good_run_list(self):
-        query = RunDBQuery(self.good_runlist)
+        query = RunDBQuery(self.good_runlist, self.cmd)
         self.assertTrue(query.get_valid_runs(1800))
 
     def test_bad_run_number(self):
-        query = RunDBQuery(self.bad_runno)
+        query = RunDBQuery(self.bad_runno, self.cmd)
         self.assertFalse(query.get_valid_runs(1800))
 
 
 if __name__ == '__main__':
     hdr_fmt = '='*5 + '{0:^{width}}' + '='*5
-    print hdr_fmt.format('RunDBQuery, rundb.RunDB backend', width=40)
+    print hdr_fmt.format('RunDBQuery, rdbt backend', width=40)
     unittest.main()
