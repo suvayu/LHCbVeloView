47dd7cdfb772a3c8ab6427ca75e2730e44be15bd
Michal Wysokinski <michal.wysokinski@cern.ch>
2014-02-04 18:35:24 +0100

    Added gui example with a smart dispatcher

---
+++ veloview/examples/guidict.py
@@ -0,0 +1,8 @@
+button_label = "label"
+histpath_label = "path"
+canvas_part = "canvas_nr"
+
+GUI_CONF_DICT = {
+    "Sine":     {histpath_label: "sinxdivx", button_label: "Draw Sin(x)/x", canvas_part: 1},
+    "Cosine":   {histpath_label: "cosxdivx", button_label: "Draw Cos(x)/x", canvas_part: 2},
+}
