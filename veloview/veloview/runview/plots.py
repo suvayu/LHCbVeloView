9a60be7976b2699f3393e635f8c51f5b49447437
Alex Pearce <alex@alexpearce.me>
2014-11-17 20:31:32 +0100

    Rename plot -> name.
    Better reflects the meaning, more compatible with past methods.

--- veloview/veloview/runview/plots.py
+++ veloview/veloview/runview/plots.py
@@ -6,17 +6,17 @@ import ROOT
 from veloview.runview import utils
 
 
-def get_run_plot(plot, run, reference=False):
+def get_run_plot(name, run, reference=False):
     """Return the object at the plot path in the run file.
 
     Keyword arguments:
-    plot -- Path within the run file to the plot object.
-            A KeyError is raised if plot is not found in the run file
+    name -- Path within the run file to the plot object.
+            A KeyError is raised if name is not found in the run file
     run -- Run number
     reference -- If True, fetch the reference plot for the given plot and run
     """
     if reference:
-        run = utils.reference_run(plot, run)
+        run = utils.reference_run(name, run)
 
     # Get the latest run file in the run's directory
     base = utils.run_file_path(run)
@@ -32,9 +32,9 @@ def get_run_plot(plot, run, reference=False):
         raise IOError("Run file not found for run {0}".format(run))
 
     # Retrieve the object
-    obj = f.Get(plot)
+    obj = f.Get(name)
     if not obj:
-        raise KeyError("Plot {0} not found in run file {1}".format(plot, run))
+        raise KeyError("Plot {0} not found in run file {1}".format(name, run))
     # The file will be closed when the function returns, so we need to clone
     # the fetched object outside the file's scope
     ROOT.gROOT.cd()
