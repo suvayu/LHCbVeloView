dc30c62e9795eb02783a5f6e36e3e8da7e33b23f
Suvayu Ali <Suvayu.Ali@cern.ch>
2014-11-18 04:35:39 +0100

    crontools: update rdbt parsing tests

--- crontools/tests/test_rundbquery.py
+++ crontools/tests/test_rundbquery.py
@@ -23,8 +23,6 @@ __hostname__ = gethostname()
 from crontools.rundbquery import (RunDBQuery)
 import unittest
 
-@unittest.skipIf(__hostname__.find('plus'),
-                 'can be tested only on plus* nodes.')
 class TestQuery(unittest.TestCase):
 
     def setUp(self):
@@ -34,21 +32,29 @@ class TestQuery(unittest.TestCase):
 
     def test_good_run_get_run_list(self):
         query = RunDBQuery(self.good_runno)
+        if __hostname__.find('plus'):
+            query._cmd_ = [os.path.dirname(__file__) + '/rdbt', '2']
         query.parse()
         self.assertTrue(query.get_valid_runs(1800))
 
     def test_good_range_get_run_list(self):
         query = RunDBQuery(self.good_runrange)
+        if __hostname__.find('plus'):
+            query._cmd_ = [os.path.dirname(__file__) + '/rdbt', '1']
         query.parse()
         self.assertTrue(query.get_valid_runs(1800))
 
     def test_filename_parsing(self):
         query = RunDBQuery(self.good_runrange)
+        if __hostname__.find('plus'):
+            query._cmd_ = [os.path.dirname(__file__) + '/rdbt', '4']
         query.parse()
         self.assertEqual(len(query.get_files(self.good_runno)), 70)
 
     def test_nonexistent_run_get_run_list(self):
         query = RunDBQuery(self.bad_runno)
+        if __hostname__.find('plus'):
+            query._cmd_ = [os.path.dirname(__file__) + '/rdbt', '2']
         query.parse()
         self.assertFalse(query.get_valid_runs(1800))
 
@@ -62,4 +68,6 @@ class TestQuery(unittest.TestCase):
 if __name__ == '__main__':
     hdr_fmt = '='*5 + '{0:^{width}}' + '='*5
     print hdr_fmt.format('RunDBQuery, rdbt backend', width=40)
+    if __hostname__.find('plus'):
+        print 'Not on a plus node, using dummy rdbt script.'
     unittest.main()
