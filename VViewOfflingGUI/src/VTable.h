1f152a092ec99ce7a7865d6fd5e23728c0eee7bb
Daniel Saunders <ds9133@samor.phy.bris.ac.uk>
2014-11-19 10:01:52 +0000

    Adding offline GUI

---
+++ VViewOfflingGUI/src/VTable.h
@@ -0,0 +1,18 @@
+#ifndef VTABLE_H
+#define VTABLE_H
+
+#include <QWidget>
+
+class VTable : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit VTable(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // VTABLE_H
